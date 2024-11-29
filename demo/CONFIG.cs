e25bd7d104b5d8bfb87bb4cc7357859a9ba7a336
AutoBuyCoin <telegramtrading@outlook.com>
2024-11-29 15:28:30 +0800

    Create CONFIG.cs

---
+++ demo/CONFIG.cs
@@ -0,0 +1,12 @@
+namespace Solana链监控指定帐号代币交易.demo
+{
+    public class CONFIG
+    {
+        public string MaxBuy { get; set; }
+        public string RpcClient { get; set; }
+        public string WWSServer { get; set; }
+        public bool SaveLog { get; set; }
+
+        public bool Play { get; set; }
+    }
+}
