85cab6456edde78ca284ec6b051865e9779f6673
AutoBuyCoin <telegramtrading@outlook.com>
2024-11-29 15:26:57 +0800

    Create MonitordAddress.cs

---
+++ models/MonitordAddress.cs
@@ -0,0 +1,10 @@
+
+namespace Solana链监控指定帐号代币交易.Model
+{
+    public class MonitoredAddress
+    {
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public int SubscriptionId { get; set; }   
+    }
+}
